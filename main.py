9ab61e813c8119566476e11829879f6eb3d264eb
Desktop <ivans200188@gmail.com>
2021-03-24 02:43:15 +0300

    changes

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from components.Market import *
-from components.Client import *
+from Market import *
 
 
 def answer_check(right_border):
 	while True:
 		try:
-			to_check = int(input(">> "))
+			to_check = int(input("=> "))
 			if right_border >= to_check >= 1:
 				return to_check
 			else:
@@ -15,28 +14,6 @@ def answer_check(right_border):
 
 
 if __name__ == "__main__":
-	# Начало программы
-	# Создаем клиентов
-	accounts = [Client("Миллионер", 100000, 10000), Client("Средний Покупатель", 500, 1000), Client("Бедный Покупатель", 20, 30)]
-	print("Вы зашли в магазин. Что вы хотите сделать? \n1. Выберите аккаунт \n2. Выйти")
-	answer = answer_check(2)
-	if answer == 1:
-		print("Доступные аккаунты: ")
-		for item in accounts:
-			print(item.name)
-		chosen_account = accounts[answer_check(len(accounts)) - 1]
-	elif answer == 2:
-		sys.exit(1)
-	print("\nАккаунт выбран. Выберите дальнейшие действия:")
-	while True:
-		print("\n1. Выбрать товары \n2. Посмотреть баланс \n3. Выйти")
-		answer = answer_check(3)
-		if answer == 1:
-			market = Market(chosen_account)
-			break
-		elif answer == 2:
-			chosen_account.show_info()
-			continue
-		elif answer == 2:
-			break
-	sys.exit(1)
+	market = Market()
+
+
